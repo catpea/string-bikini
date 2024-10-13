12e1ab6ba6d48831a4b642bbdf502ab397b1deea
catpea <corporate.miou@gmail.com>
2024-10-13 14:01:30 -0400

    New Release

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ export function quot(input) {
   return JSON.stringify(input)
 }
 
-export function toClass(str, upper=true) {
-  return str
-  .split(/[^a-z0-1)]/i) // Split by spaces
-  .map(word => (upper?word.charAt(0).toUpperCase():word.charAt(0).toLowerCase()) + word.slice(1).toLowerCase()) // Capitalize first letter
+export function toClass(str, lower=true) {
+  console.log([...str.split(/[^a-z0-1)]/i).entries()]);
+  return [...str.split(/[^a-z0-1)]/i).entries()]
+  .map(([index, word]) => (index==0&&lower?word.charAt(0).toLowerCase():word.charAt(0).toUpperCase()) + word.slice(1).toLowerCase()) // Capitalize first letter
   .join(''); // Join without spaces
 }
 
 export function toInstance(str) {
-  return toClass(str, false)
+  return toClass(str, true)
 }
 
 export function toProperty(str) {
